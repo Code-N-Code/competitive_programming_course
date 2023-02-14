6ad50a6a3731d13e750c0b8e59ff12927696621a
Istekhar Ahmad <codeahmad51@gmail.com>
2023-02-14 09:58:35 +0530

    added mex problem solution

---
+++ Codeforces/Global Rounds/Round 9/D. Replace by MEX.cpp
@@ -0,0 +1,56 @@
+// Problem Link : https://codeforces.com/contest/1375/problem/D
+// Video solution link : 
+// Write here if video solution doesn't exist : icodencode@gmail.com
+#include<bits/stdc++.h>
+#define vi vector<int>
+#define ll long long
+using namespace std;
+
+int ar[1000];
+
+int findMex(int n){
+	set<int> s(ar + 0 , ar + n);
+	
+	int mex = 0;
+	
+	while(s.count(mex)) mex++;
+	
+	return mex;
+}
+
+int getInvalidIndex(int n){
+	for(int i=0;i<n;i++) if(ar[i] != i) return i;
+	
+	return n-1;
+}
+
+int  main(){
+	int t , n;
+	
+	cin>>t;
+	
+	while(t--){
+		cin>>n;
+		for(int i=0;i<n;i++) cin>>ar[i];
+		
+		vi result;
+		
+		for(int i=1;i<=n;i++){
+			int mex = findMex(n);
+			
+			if(mex == n){
+				int index = getInvalidIndex(n);
+				ar[index] = mex;
+				result.push_back(index);
+				mex = findMex(n);
+			}
+			
+			ar[mex] = mex;
+			result.push_back(mex);
+		}
+		
+		cout<<result.size()<<endl;
+		for(int idx : result) cout<<idx + 1<<" ";
+		cout<<endl;
+	}
+}
